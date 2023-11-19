d00d022d067c0f26ef2d17c0d9e75f989bd72240
Colin Evans <colinevans@colins-mbp.home>
2023-11-18 19:47:53 -0500

    Adding extension for Resource on the JSON data type

--- Networking/Sources/Networking/Resource.swift
+++ Networking/Sources/Networking/Resource.swift
@@ -12,4 +12,18 @@ import Foundation
 public struct Resource<A> {
   let url: URL
   let parse: (Data) throws -> A
+  
+  init(url: URL, parse: @escaping (Data) throws -> A) {
+    self.url = url
+    self.parse = parse
+  }
+}
+
+extension Resource where A: Codable {
+  init(json url: URL) {
+    let parse = {
+      try JSONDecoder().decode(A.self, from: $0)
+    }
+    self.init(url: url, parse: parse)
+  }
 }
